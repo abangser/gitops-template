d4610d0641f74fdbb156a6e10e6bb1f256c41860
Abby Bangser <abby@syntasso.io>
2023-06-04 21:09:52 +0100

    remove extra installs

--- digitalocean-github/terraform/github/ssh.tf
+++
@@ -1,9 +0,0 @@
-resource "github_user_ssh_key" "kbot" {
-  title = "kbot-<CLUSTER_NAME>"
-  key   = var.kbot_ssh_public_key
-}
-
-variable "kbot_ssh_public_key" {
-  type    = string
-  default = ""
-}
